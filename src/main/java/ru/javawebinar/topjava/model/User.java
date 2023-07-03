98f7c28901b970a285c5a635b7c5ae4d69023565
Marten09 <leshadyuckarev631@gmail.com>
2023-07-03 13:49:21 +0300

    HW5

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -53,6 +53,9 @@ public class User extends AbstractNamedEntity {
     @Column(name = "calories_per_day", nullable = false, columnDefinition = "int default 2000")
     @Range(min = 10, max = 10000)
     private int caloriesPerDay = DEFAULT_CALORIES_PER_DAY;
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @OrderBy("dateTime desc")
+    private List<Meal> meals;
 
     public User() {
     }
@@ -121,6 +124,14 @@ public class User extends AbstractNamedEntity {
 
     public String getPassword() {
         return password;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
     }
 
     @Override
---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.*;
+
+@ActiveProfiles(Profiles.DATAJPA)
+public class DataJpaMealServiceTest extends MealServiceTest {
+    @Test
+    public void getWithUser() {
+        Meal actual = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
+        MEAL_MATCHER.assertMatch(actual, adminMeal1);
+        USER_MATCHER.assertMatch(actual.getUser(), admin);
+    }
+
+    @Test
+    public void getWithUserNotFound() {
+        assertThrows(NotFoundException.class, () -> service.getWithUser(MealTestData.NOT_FOUND, USER_ID));
+    }
+
+    @Test
+    public void getWithUserNotOwn() {
+        assertThrows(NotFoundException.class, () -> service.getWithUser(ADMIN_MEAL_ID, USER_ID));
+    }
+}
